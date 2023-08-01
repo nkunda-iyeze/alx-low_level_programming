838f1b8cfa158b7fff2387e9da76c0cda82abd78
nkunda-iyeze <shalom.iyeze@gmail.com>
2023-07-31 19:40:14 -0700

    Completion of task 0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *set_string - sets a value of a pointer to char
+ *@s:pointer to pointer
+ *@to:pinter to char
+ *
+ */
+
+
+void set_string(char **s, char *to)
+{ *s = to}
